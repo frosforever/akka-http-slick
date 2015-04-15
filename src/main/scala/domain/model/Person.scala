46711136ce3b77f7424ab320c9ac91beb36783ef
frosforever <frosforever@users.noreply.github.com>
2015-04-15 16:04:31 -0400

    Add basic persistance

---
+++ src/main/scala/domain/model/Person.scala
@@ -0,0 +1,5 @@
+package domain.model
+
+case class Person(id: Long, name: String, age: Int) {
+
+}
---
+++ src/main/scala/domain/persistance/Tables.scala
@@ -0,0 +1,20 @@
+package domain.persistance
+
+import domain.model.Person
+import slick.driver.H2Driver.api._
+
+class Persons(tag: Tag) extends Table[Person](tag, "PERSONS") {
+
+  def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
+
+  def name = column[String]("NAME")
+
+  def age = column[Int]("AGE")
+
+  def * = (id.?, name, age) <>(Person.tupled, Person.unapply)
+}
+
+object Tables {
+  val persons = TableQuery[Persons]
+}
+
